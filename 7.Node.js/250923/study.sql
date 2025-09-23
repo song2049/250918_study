00479841f518c9080295450dd81270b5361d7f53
song2049 <song912049@gmail.com>
2025-09-23 17:27:24 +0900

    feat: board 테이블 생성, 요구사항 맞는 값으로 설정

--- 7.Node.js/250923/study.sql
+++ 7.Node.js/250923/study.sql
@@ -4,3 +4,23 @@ mysql -u root -p
 
 mysql> CREATE DATABASE study;
 Query OK, 1 row affected (0.00 sec)
+
+mysql> DROP DATABASE study
+mysql> SHOW DATABASES;
+
+mysql> CREATE DATABASE boards2;
+Query OK, 1 row affected (0.01 sec)
+
+mysql> use boards2;
+Database changed
+
+CREATE TABLE board (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    user_id VARCHAR(50) NOT NULL,
+    title VARCHAR(150) NOT NULL,
+    content TEXT,
+    created_at DATETIME NOT NULL DEFAULT NOW(),
+    hit INT(11) DEFAULT 0,
+    PRIMARY KEY (id)
+);
+
